e30a48a852a2a5d5801d9e43b04f29e9c87ea1eb
jensen-westraad <jensenwestraad@gmail.com>
2025-07-04 19:12:55 +0200

    Completed Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("What is your First Name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your Last Name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
+
+
 }
+
