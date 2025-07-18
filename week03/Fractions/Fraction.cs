d32b0d649224d305e2a3b21464ccd8eb87e20f34
jensen-westraad <jensenwestraad@gmail.com>
2025-07-18 20:38:50 +0200

    fixed other class errors

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -1,18 +1,18 @@
 using System;
 
-public class Fractions
+public class Fraction
 {
     private int _top;
 
     private int _bottom;
 
-    public Fractions()
+    public Fraction()
     {
         _top = -1;
         _bottom = 1;
-    } 
+    }
 
-     public Fraction(int wholeNumber)
+    public Fraction(int wholeNumber)
     {
         _top = wholeNumber;
         _bottom = 1;
@@ -34,6 +34,6 @@ public class Fractions
     {
         return (double)_top / (double)_bottom;
     }
-   
+
 
 }
