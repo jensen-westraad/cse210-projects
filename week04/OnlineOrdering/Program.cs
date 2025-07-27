11ac7b8b260bba6c530db9d5bb53ebdb886cb2cb
jensen-westraad <jensenwestraad@gmail.com>
2025-07-27 10:53:55 +0200

    finished program class to test my shopping classes

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("123 Peach Rd", "Atlanta", "GA", "USA");
+        Customer customer1 = new Customer("Kyle West", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Pencil", "P001", 0.50, 10));
+        order1.AddProduct(new Product("Eraser", "E002", 1.00, 2));
+
+        Address address2 = new Address("55 Old Trafford St", "London", "England", "UK");
+        Customer customer2 = new Customer("Sam Parker", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Marker", "M003", 2.00, 4));
+        order2.AddProduct(new Product("Sticky Notes", "S004", 1.25, 5));
+        order2.AddProduct(new Product("Prestick", "P005", 3.50, 1));
+
+        DisplayOrder(order1);
+        Console.WriteLine("--------------------------");
+        DisplayOrder(order2);
+    }
+
+    static void DisplayOrder(Order order)
+    {
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine("Total Cost: $" + order.GetTotalCost().ToString("0.00"));
     }
 }
