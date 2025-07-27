72bfffa010190f45ea702e1c74d1f7b032f881ae
jensen-westraad <jensenwestraad@gmail.com>
2025-07-27 10:58:49 +0200

    small changes to items

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -8,16 +8,16 @@ class Program
         Customer customer1 = new Customer("Kyle West", address1);
 
         Order order1 = new Order(customer1);
-        order1.AddProduct(new Product("Pencil", "P001", 0.50, 10));
+        order1.AddProduct(new Product("Pencil", "P006", 0.50, 10));
         order1.AddProduct(new Product("Eraser", "E002", 1.00, 2));
 
         Address address2 = new Address("55 Old Trafford St", "London", "England", "UK");
         Customer customer2 = new Customer("Sam Parker", address2);
 
         Order order2 = new Order(customer2);
-        order2.AddProduct(new Product("Marker", "M003", 2.00, 4));
-        order2.AddProduct(new Product("Sticky Notes", "S004", 1.25, 5));
-        order2.AddProduct(new Product("Prestick", "P005", 3.50, 1));
+        order2.AddProduct(new Product("Lamp", "L003", 2.00, 4));
+        order2.AddProduct(new Product("Desk", "D009", 1.25, 5));
+        order2.AddProduct(new Product("Poster", "P005", 3.50, 1));
 
         DisplayOrder(order1);
         Console.WriteLine("--------------------------");
